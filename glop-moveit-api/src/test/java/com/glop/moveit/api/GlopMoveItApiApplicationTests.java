5e1da5fc44b662f9bde99d2c9f8701743a9d7cee
Julien-Conoir <julien.conoir.etu@univ-lille.fr>
2025-09-28 22:01:06 +0200

    fix: exclude DataSource and Hibernate JPA auto-configuration in Spring Boot tests

--- glop-moveit-api/src/test/java/com/glop/moveit/api/GlopMoveItApiApplicationTests.java
+++ glop-moveit-api/src/test/java/com/glop/moveit/api/GlopMoveItApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.glop.moveit.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootTest
+@SpringBootTest(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 class GlopMoveItApiApplicationTests {
 
     @Test
