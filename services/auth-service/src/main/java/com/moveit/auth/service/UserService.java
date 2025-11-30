e573eae0a401b52ac0494cc23a6500e184798f71
Julien Conoir <julien.conoir.etu@univ-lille.fr>
2025-11-30 22:27:30 +0100

    feat: add UserRepository and UserService for user management

---
+++ services/auth-service/src/main/java/com/moveit/auth/service/UserService.java
@@ -0,0 +1,75 @@
+package com.moveit.auth.service;
+
+import com.moveit.auth.entity.Role;
+import com.moveit.auth.entity.RoleEnum;
+import com.moveit.auth.entity.User;
+import com.moveit.auth.repository.UserRepository;
+import com.moveit.auth.model.RegisterUserDto;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Service de gestion des utilisateurs.
+ */
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final RoleService roleService;
+    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * Initialise le super administrateur au démarrage de l'application.
+     */
+    @PostConstruct
+    void init(){
+        RegisterUserDto userDto = new RegisterUserDto("super.admin@email.com","123456","Super Admin");
+
+        Optional<Role> optionalRole = roleService.findByName(RoleEnum.SUPER_ADMIN);
+        Optional<User> optionalUser = userRepository.findByEmail(userDto.email());
+
+        if (optionalRole.isEmpty() || optionalUser.isPresent()) {
+            return;
+        }
+
+        var user = new User()
+                .setFullName(userDto.fullName())
+                .setEmail(userDto.email())
+                .setPassword(passwordEncoder.encode(userDto.password()))
+                .setRole(optionalRole.get());
+
+        userRepository.save(user);
+    }
+
+    /**
+     * Récupère la liste de tous les utilisateurs.
+     */
+    public List<User> allUsers() {
+        return userRepository.findAll();
+    }
+
+    /**
+     * Crée un nouvel administrateur.
+     */
+    public User createAdministrator(RegisterUserDto input) {
+        Optional<Role> optionalRole = roleService.findByName(RoleEnum.ADMIN);
+
+        if (optionalRole.isEmpty()) {
+            return null;
+        }
+
+        var user = new User()
+                .setFullName(input.fullName())
+                .setEmail(input.email())
+                .setPassword(passwordEncoder.encode(input.password()))
+                .setRole(optionalRole.get());
+
+        return userRepository.save(user);
+    }
+}
