f986e2888a72b5c625f80d05f46c0a896bcf9376
Julien-Conoir <julien.conoir.etu@univ-lille.fr>
2025-12-29 16:11:15 +0100

    feat: enhance API documentation with OpenAPI annotations for authentication and user management endpoints

--- auth-service/src/main/java/com/moveit/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/moveit/auth/controller/AuthController.java
@@ -6,6 +6,11 @@ import com.moveit.auth.model.LoginUserDto;
 import com.moveit.auth.model.RegisterUserDto;
 import com.moveit.auth.service.AuthenticationService;
 import com.moveit.auth.service.JwtService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,9 +18,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Contrôleur de gestion de l'authentification.
- */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
@@ -24,18 +26,25 @@ public class AuthController {
     private final JwtService jwtService;
     private final AuthenticationService authenticationService;
 
-    /**
-     * Endpoint d'inscription d'un nouvel utilisateur.
-     */
+    @Operation(summary = "Inscription d'un utilisateur")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Utilisateur inscrit avec succès", content = @Content(schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "400", description = "Données d'inscription invalides"),
+            @ApiResponse(responseCode = "409", description = "L'utilisateur existe déjà"),
+            @ApiResponse(responseCode = "500", description = "Erreur interne du serveur")
+    })
     @PostMapping("/signup")
     public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
         User registeredUser = authenticationService.signup(registerUserDto);
         return ResponseEntity.ok(registeredUser);
     }
 
-    /**
-     * Endpoint d'authentification.
-     */
+    @Operation(summary = "Authentification d'un utilisateur")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Utilisateur authentifié avec succès", content = @Content(schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "401", description = "Échec de l'authentification"),
+            @ApiResponse(responseCode = "500", description = "Erreur interne du serveur")
+    })
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
         var authenticatedUser = authenticationService.authenticate(loginUserDto);
--- auth-service/src/main/java/com/moveit/auth/controller/UserController.java
+++ auth-service/src/main/java/com/moveit/auth/controller/UserController.java
@@ -3,6 +3,11 @@ package com.moveit.auth.controller;
 import com.moveit.auth.entity.User;
 import com.moveit.auth.model.RegisterUserDto;
 import com.moveit.auth.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
@@ -15,9 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-/**
- * Contrôleur de gestion des utilisateurs.
- */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
@@ -25,9 +27,12 @@ public class UserController {
 
     private final UserService userService;
 
-    /**
-     * Endpoint pour récupérer les informations de l'utilisateur authentifié.
-     */
+    @Operation(summary = "Récupération de l'utilisateur authentifié")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Récupération de l'utilisateur authentifié avec succès", content = @Content(schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "401", description = "Utilisateur non authentifié"),
+            @ApiResponse(responseCode = "500", description = "Erreur interne du serveur")
+    })
     @GetMapping("/me")
     @PreAuthorize("isAuthenticated()")
     public User authenticatedUser(){
@@ -35,18 +40,25 @@ public class UserController {
         return (User) authentication.getPrincipal();
     }
 
-    /**
-     * Endpoint pour récupérer la liste de tous les utilisateurs.
-     */
+    @Operation(summary = "Récupération de tous les utilisateurs")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Récupération de tous les utilisateurs avec succès", content = @Content(schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "403", description = "Accès refusé"),
+            @ApiResponse(responseCode = "500", description = "Erreur interne du serveur")
+    })
     @GetMapping()
     @PreAuthorize("hasAnyRole('ADMIN', 'COMMISSIONER')")
     public List<User> allUsers() {
         return userService.allUsers();
     }
 
-    /**
-     * Endpoint pour créer un nouvel administrateur.
-     */
+    @Operation(summary = "Création d'un administrateur")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Administrateur créé avec succès", content = @Content(schema = @Schema(implementation = User.class))),
+            @ApiResponse(responseCode = "400", description = "Données d'inscription invalides"),
+            @ApiResponse(responseCode = "409", description = "L'utilisateur existe déjà"),
+            @ApiResponse(responseCode = "500", description = "Erreur interne du serveur")
+    })
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     public User createAdministrator(@RequestBody RegisterUserDto registerUserDto) {
